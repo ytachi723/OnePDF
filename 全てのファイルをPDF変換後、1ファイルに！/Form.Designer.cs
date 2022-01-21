146d978647c993de94641121ee4877bb65f8df05
Tachi <ytachi723@gmail.com>
2022-01-21 17:34:57 +0900

    All 20220121

---
+++ 全てのファイルをPDF変換後、1ファイルに！/Form.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace 全てのファイルをPDF変換後_1ファイルに_
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.textbox = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.progress = new System.Windows.Forms.Label();
+            this.extension = new System.Windows.Forms.Label();
+            this.memo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label.Location = new System.Drawing.Point(12, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(148, 16);
+            this.label.TabIndex = 0;
+            this.label.Text = "完成後のファイル名：";
+            // 
+            // textbox
+            // 
+            this.textbox.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textbox.Location = new System.Drawing.Point(159, 6);
+            this.textbox.Name = "textbox";
+            this.textbox.Size = new System.Drawing.Size(386, 23);
+            this.textbox.TabIndex = 1;
+            this.textbox.TextChanged += new System.EventHandler(this.textbox_TextChanged);
+            // 
+            // button
+            // 
+            this.button.Enabled = false;
+            this.button.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button.Location = new System.Drawing.Point(75, 44);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 2;
+            this.button.Text = "開始";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // progress
+            // 
+            this.progress.AutoSize = true;
+            this.progress.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.progress.Location = new System.Drawing.Point(156, 47);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(65, 16);
+            this.progress.TabIndex = 3;
+            this.progress.Text = "progress";
+            // 
+            // extension
+            // 
+            this.extension.AutoSize = true;
+            this.extension.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.extension.Location = new System.Drawing.Point(541, 13);
+            this.extension.Name = "extension";
+            this.extension.Size = new System.Drawing.Size(36, 16);
+            this.extension.TabIndex = 4;
+            this.extension.Text = ".pdf";
+            // 
+            // memo
+            // 
+            this.memo.AutoSize = true;
+            this.memo.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.memo.Location = new System.Drawing.Point(72, 70);
+            this.memo.Name = "memo";
+            this.memo.Size = new System.Drawing.Size(31, 16);
+            this.memo.TabIndex = 5;
+            this.memo.Text = "メモ";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 93);
+            this.Controls.Add(this.memo);
+            this.Controls.Add(this.textbox);
+            this.Controls.Add(this.extension);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.label);
+            this.Name = "Form";
+            this.Text = "全てのファイルをPDF変換後、1つのファイルに！";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textbox;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label progress;
+        private System.Windows.Forms.Label extension;
+        private System.Windows.Forms.Label memo;
+    }
+}
+
---
+++ 全てのファイルをPDF変換後、1ファイルに！/Form.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+
+
+namespace 全てのファイルをPDF変換後_1ファイルに_
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+            progress.Text = "";
+            memo.Text = "";
+        }
+
+        private void textbox_TextChanged(object sender, EventArgs e)
+        {
+            if (textbox.Text.Length > 0)
+            {
+                button.Enabled = true;
+            }
+            else
+            {
+                button.Enabled = false;
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (!button.Enabled)
+            {
+                return;
+            }
+
+            System.IO.DirectoryInfo nowFolder = new System.IO.DirectoryInfo(@".");
+            System.IO.SearchOption searchOption = System.IO.SearchOption.TopDirectoryOnly;
+
+            // WordファイルをPDFファイルに変換
+            Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+            Microsoft.Office.Interop.Word.Document document = null;
+            IEnumerable<System.IO.FileInfo> now = nowFolder.EnumerateFiles("*.docx", searchOption);
+
+            foreach (var item in now)
+            {
+                progress.Text = item + " を変換中";
+                try
+                {
+                    document = word.Documents.Open(item.DirectoryName + "\\" + item);
+                    document.ExportAsFixedFormat(item.DirectoryName + "\\" + item + ".pdf", Microsoft.Office.Interop.Word.WdExportFormat.wdExportFormatPDF);
+                    document.Close();
+                }
+                catch (System.Exception exception)
+                {
+                    System.Console.WriteLine(exception.Message);
+                }
+            }
+
+            // PowerPointファイルをPDFファイルに変換
+            // PowerPointがどうしても開いたままになる by tachi 2021/07/10
+            // open関数の引数指定でPowerPointが開いたままになることを解消 by tachi 2021/08/19
+            Microsoft.Office.Interop.PowerPoint.Application powerpoint = new Microsoft.Office.Interop.PowerPoint.Application();
+            Microsoft.Office.Interop.PowerPoint.Presentation presentation = null;
+
+            now = nowFolder.EnumerateFiles("*.pptx", searchOption);
+            // PowerPointが開いたままにならなくなったため、削除 by tachi 2021/08/19
+            //            var pptFlag = (now.Count() > 0);
+            //            if (pptFlag)
+            //            {
+            //                memo.Text = "PowerPointが開いたままになるので、必要に応じて閉じてください。";
+            //            }
+            foreach (var item in now)
+            {
+                progress.Text = item + " を変換中";
+                try
+                {
+                    presentation = powerpoint.Presentations.Open(item.DirectoryName + "\\" + item, Microsoft.Office.Core.MsoTriState.msoTrue, Microsoft.Office.Core.MsoTriState.msoTrue, Microsoft.Office.Core.MsoTriState.msoFalse);
+                    presentation.ExportAsFixedFormat(item.DirectoryName + "\\" + item + ".pdf", Microsoft.Office.Interop.PowerPoint.PpFixedFormatType.ppFixedFormatTypePDF);
+                    presentation.Close();
+                }
+                catch (System.Exception exception)
+                {
+                    System.Console.WriteLine(exception.Message);
+                }
+            }
+
+            // PDFファイルを統合
+            progress.Text = textbox.Text + ".pdf を作成中";
+            now = nowFolder.EnumerateFiles("*.pdf", searchOption);
+            PdfDocument pdfDocument = new PdfDocument();
+            PdfDocument inputDocument;
+            foreach (var item in now)
+            {
+                inputDocument = PdfReader.Open(item.DirectoryName + "\\" + item.ToString(), PdfDocumentOpenMode.Import);
+                foreach (PdfPage page in inputDocument.Pages)
+                {
+                    // PDF頁を追加
+                    pdfDocument.AddPage(page);
+                }
+                inputDocument.Close();
+            }
+            pdfDocument.Save(now.First().DirectoryName + "\\" + textbox.Text + ".pdf");
+            // PDFを閉じる
+            pdfDocument.Close();
+
+            progress.Text = "完了";
+            // PowerPointが開いたままにならなくなったため、削除 by tachi 2021/08/19
+            //            if (pptFlag)
+            //            {
+            //                MessageBox.Show("PowerPointが開いたままになるので、必要に応じて閉じてください。");
+            //            }
+            Application.Exit();
+        }
+    }
+}
